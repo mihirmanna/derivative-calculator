e66a8738624e623fb714e66ba9c06b2203981d44
Mihir Manna <mihirmanna2@gmail.com>
2024-02-17 18:49:18 -0600

    Added string test for nested equation

--- test_derivative.py
+++ test_derivative.py
@@ -4,26 +4,35 @@ from derivative import *
 
 class TestStringMethods(unittest.TestCase):
     def setUp(self):
-        self.func1 = Function(FunTyp.POLY, 3, 8)
-        self.func2 = Function(FunTyp.EXP, 2, 7)
-        self.func3 = Function(FunTyp.LOG, 6, 5)
-        self.func4 = Function(FunTyp.SIN, 10, 4)  # b argument is unused
-        self.func5 = Function(FunTyp.COS, 9, 1)  # b argument is unused
+        self.poly_eqn = Function(FunTyp.POLY, 3, 8)
+        self.exp_eqn = Function(FunTyp.EXP, 2, 7)
+        self.log_eqn = Function(FunTyp.LOG, 6, 5)
+        self.sin_eqn = Function(FunTyp.SIN, 10)
+        self.cos_eqn = Function(FunTyp.COS, 9)
+        self.nested_eqn = Function(FunTyp.POLY, 1, 2, [
+            Function(FunTyp.SIN, 1, inside=[
+                Function(FunTyp.POLY, 5, 1),
+                Function(FunTyp.POLY, 1, 0)
+            ])
+        ])
 
     def test_polynomial(self):
-        self.assertEqual(str(self.func1), '3(x)^8')
+        self.assertEqual(str(self.poly_eqn), '3(x)^8')
 
     def test_exponential(self):
-        self.assertEqual(str(self.func2), '2*7^(x)')
+        self.assertEqual(str(self.exp_eqn), '2*7^(x)')
 
     def test_logarithm(self):
-        self.assertEqual(str(self.func3), '6log_5(x)')
+        self.assertEqual(str(self.log_eqn), '6log_5(x)')
     
     def test_sine(self):
-        self.assertEqual(str(self.func4), '10sin(x)')
+        self.assertEqual(str(self.sin_eqn), '10sin(x)')
     
     def test_cosine(self):
-        self.assertEqual(str(self.func5), '9cos(x)')
+        self.assertEqual(str(self.cos_eqn), '9cos(x)')
+
+    def test_nested(self):
+        self.assertEqual(str(self.nested_eqn), '1(1sin(5(x)^1 + 1(x)^0 + ) + )^2')
 
 
 if __name__ == '__main__':
