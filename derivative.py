0e7f7f7075bffef7e04ec26efb524edeedffa12c
Mihir Manna <mihirmanna2@gmail.com>
2024-02-17 19:16:13 -0600

    Removed coefficients from all functions except polynomials

--- derivative.py
+++ derivative.py
@@ -3,14 +3,14 @@ from enum import Enum
 
 class FunTyp(Enum):
     POLY = 1  # a = coefficient, b = exponent
-    EXP = 2  # a = coefficient, b = base
-    LOG = 3  # a = coefficient, b = base
-    SIN = 4  # a = coefficient
-    COS = 5  # a = coefficient
+    EXP = 2  # a = base
+    LOG = 3  # a = base
+    SIN = 4
+    COS = 5
 
 
 class Function:
-    def __init__(self, function_type: FunTyp, a: float, b: float = 0, inside: list = None):
+    def __init__(self, function_type: FunTyp, a: float = 0, b: float = 0, inside: list = None):
         self.type = function_type
         self.a = a
         self.b = b
@@ -30,18 +30,14 @@ class Function:
                 to_return += f"^{self.b}"
 
         elif self.type == FunTyp.EXP:
-            if not self.a == 1:
-                to_return += f"{self.a}*"
-            to_return += f"{self.b}^"
+            to_return += f"{self.a}^"
             if self.inside is not None:
                 to_return += f'({"+".join(list(map(lambda x: str(x), self.inside)))})'
             else:
                 to_return += "x"
 
         elif self.type == FunTyp.LOG:
-            if not self.a == 1:
-                to_return += f"{self.a}"
-            to_return += f"log_{self.b}("
+            to_return += f"log_{self.a}("
             if self.inside is not None:
                 to_return += "+".join(list(map(lambda x: str(x), self.inside)))
             else:
@@ -49,8 +45,6 @@ class Function:
             to_return += ")"
 
         elif self.type == FunTyp.SIN:
-            if not self.a == 1:
-                to_return += f"{self.a}"
             to_return += "sin("
             if self.inside is not None:
                 to_return += "+".join(list(map(lambda x: str(x), self.inside)))
@@ -59,8 +53,6 @@ class Function:
             to_return += ")"
 
         elif self.type == FunTyp.COS:
-            if not self.a == 1:
-                to_return += f"{self.a}"
             to_return += "cos("
             if self.inside is not None:
                 to_return += "+".join(list(map(lambda x: str(x), self.inside)))
@@ -69,11 +61,6 @@ class Function:
             to_return += ")"
 
         return to_return
-
-
-def power_rule(func: Function):
-    func.a *= func.b
-    func.b -= 1
 
 
 if __name__ == "__main__":
--- test_derivative.py
+++ test_derivative.py
@@ -5,12 +5,12 @@ from derivative import *
 class TestStringMethods(unittest.TestCase):
     def setUp(self):
         self.poly_eqn = Function(FunTyp.POLY, 3, 8)
-        self.exp_eqn = Function(FunTyp.EXP, 2, 7)
-        self.log_eqn = Function(FunTyp.LOG, 6, 5)
-        self.sin_eqn = Function(FunTyp.SIN, 10)
-        self.cos_eqn = Function(FunTyp.COS, 9)
+        self.exp_eqn = Function(FunTyp.EXP, 2)
+        self.log_eqn = Function(FunTyp.LOG, 6)
+        self.sin_eqn = Function(FunTyp.SIN)
+        self.cos_eqn = Function(FunTyp.COS)
         self.nested_eqn = Function(FunTyp.POLY, 1, 2, [
-            Function(FunTyp.SIN, 1, inside=[
+            Function(FunTyp.SIN, inside=[
                 Function(FunTyp.POLY, 5, 1),
                 Function(FunTyp.POLY, 1, 0)
             ])
@@ -20,16 +20,16 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(str(self.poly_eqn), '3x^8')
 
     def test_exponential(self):
-        self.assertEqual(str(self.exp_eqn), '2*7^x')
+        self.assertEqual(str(self.exp_eqn), '2^x')
 
     def test_logarithm(self):
-        self.assertEqual(str(self.log_eqn), '6log_5(x)')
+        self.assertEqual(str(self.log_eqn), 'log_6(x)')
     
     def test_sine(self):
-        self.assertEqual(str(self.sin_eqn), '10sin(x)')
+        self.assertEqual(str(self.sin_eqn), 'sin(x)')
     
     def test_cosine(self):
-        self.assertEqual(str(self.cos_eqn), '9cos(x)')
+        self.assertEqual(str(self.cos_eqn), 'cos(x)')
 
     def test_nested(self):
         self.assertEqual(str(self.nested_eqn), '(sin(5x+x^0))^2')
